2a4fe0c4e1989b5f2c5e7d1d67263ee52471fa5e
aliev008 <hfe.as@yandex.ru>
2022-08-24 15:33:17 +0300

    fix: optimized modal window script

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,12 +1,18 @@
 const modals = () => {
-  function bindModal(trigger, modal, close) {
-    trigger.addEventListener("click", (e) => {
-      if (e.target) {
-        e.preventDefault();
-      }
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector);
 
-      modal.style.display = "block";
-      document.body.style.overflow = "hidden";
+    trigger.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+      });
     });
 
     close.addEventListener("click", () => {
@@ -22,11 +28,11 @@ const modals = () => {
     });
   }
 
-  const callEngineerBtn = document.querySelector(".popup_engineer_btn"),
-    modalEngineer = document.querySelector(".popup_engineer"),
-    modalEngineerClose = document.querySelector(".popup_engineer .popup_close");
-
-  bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+  bindModal(
+    ".popup_engineer_btn",
+    ".popup_engineer",
+    ".popup_engineer .popup_close"
+  );
 };
 
 export default modals;
