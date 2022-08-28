4af380df3d42e1e4a2be1b12004f76bcf1e57a33
aliev008 <hfe.as@yandex.ru>
2022-08-28 15:05:09 +0300

    fix: syntax errors fix

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,64 +1,61 @@
 export const forms = () => {
-    const pageForms = document.querySelectorAll("form"),
-          inputs = document.querySelectorAll("input");
-          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+  const pageForms = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input");
+  phoneInputs = document.querySelectorAll('input[name="user_phone"]');
 
-    phoneInputs.forEach((phoneInput) => {
-        phoneInput.addEventListener('input', (e) => {
-            phoneInput.value = phoneInput.value.replace(/\D/, '');
+  phoneInputs.forEach((phoneInput) => {
+    phoneInput.addEventListener("input", (e) => {
+      phoneInput.value = phoneInput.value.replace(/\D/, "");
+    });
+  });
+
+  const message = {
+    loading: "Загрузка...",
+    success: "Спасибо! Наши специалисты свяжутся с Вами в ближайшее время",
+    failue: "Что-то пошло не так :-(",
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector(".status").textContent = message.loading;
+    let res = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+
+    return await res.text();
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+  };
+
+  pageForms.forEach((form) => {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      form.appendChild(statusMessage);
+
+      const formData = new FormData(form);
+
+      postData("assets/server.php", formData)
+        .then((result) => {
+          console.log(result);
+          statusMessage.textContent = message.success;
         })
-    })
-
-    const message = {
-        loading: "Загрузка...",
-        success: "Спасибо! Наши специалисты свяжутся с Вами в ближайшее время",
-        failue: "Что-то пошло не так :-("
-    }
-
-    const postData = async (url, data) => {
-        document.querySelector('.status').textContent = message.loading;
-        let res = await fetch(url, {
-            method: "POST",
-            body: data;
-
+        .catch((error) => {
+          console.log(error);
+          statusMessage.textContent = message.failure;
+        })
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
         });
-
-        return await res.text();
-    }
-
-    const clearInputs = () => {
-        inputs.forEach((input) => {
-            input.value = '';
-        })
-    }
-
-    pageForms.forEach((form) => {
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-
-            let statusMessage = document.createElement('div');
-            statusMessage.classList.add("status");
-            form.appendChild(statusMessage);
-
-            const formData = new FormData(form)
-
-            postData("assets/server.php", formData)
-                .then(result => {
-                    console.log(result);
-                    statusMessage.textContent = message.success;
-                })
-                .catch((error) => {
-                    console.log(error);
-                    statusMessage.textContent = message.failure;
-                })
-                .finally(() => {
-                    clearInputs();
-                    setTimeout(() => {
-                        statusMessage.remove()
-                    }, 5000);
-                })
-        })
-    })
-
-
-}
+    });
+  });
+};
