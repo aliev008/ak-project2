3fbbb9299cbf59a365a8c1768f781a69bd4f6622
aliev008 <hfe.as@yandex.ru>
2022-10-01 11:44:45 +0300

    feat: impemented popup action on "Наши Работы" section

---
+++ src/js/modules/images.js
@@ -0,0 +1,33 @@
+export const images = () => {
+  const imgPopup = document.createElement("div"),
+    workSection = document.querySelector(".works"),
+    bigImage = document.createElement("img");
+
+  workSection.appendChild(imgPopup);
+  imgPopup.appendChild(bigImage);
+
+  imgPopup.classList.add("popup");
+  imgPopup.style.justifyContent = "center";
+  imgPopup.style.alignItems = "center";
+  imgPopup.style.display = "none";
+
+  workSection.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e);
+
+    let target = e.target;
+
+    if (target && target.classList.contains("preview")) {
+      imgPopup.style.display = "flex";
+      const path = target.parentNode.getAttribute("href");
+      bigImage.setAttribute("src", path);
+      bigImage.classList.add("works_bigImg");
+      document.body.style.overflow = "hidden";
+    }
+
+    if (target && target.matches("div.popup")) {
+      imgPopup.style.display = "none";
+      document.body.style.overflow = "";
+    }
+  });
+};
